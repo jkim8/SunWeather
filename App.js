aeff05996f6b6794a57405e6d0a3c94ae9e47582
jkim8 <juno0218@naver.com>
2022-08-02 17:51:29 -0400

    style

--- App.js
+++ App.js
@@ -7,9 +7,14 @@ export default function App() {
     <View style={styles.container}>
     <StatusBar style="light"/>
     <View style={styles.city}>
-      <Text>Atlanta</Text>
+      <Text style={styles.cityName}>Atlanta</Text>
     </View>
-    <View style={styles.weather}></View>
+    <View style={styles.weather}>
+      <View style={styles.day}>
+        <Text style={styles.temp}>27</Text>
+        <Text style={styles.description}>Sunny</Text>
+      </View>
+    </View>
     </View>
   );
 }
@@ -20,13 +25,30 @@ const styles = StyleSheet.create({
     backgroundColor: "tomato"
   },
   city: {
-    flex: 1,
-    backgroundColor: 'blue',
-    justifyContent: 'center'
+    flex: 1.2,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  cityName: {
+    color: 'white',
+    fontSize: 68,
+    fontWeight: '500'
   },
   weather: {
     flex: 3,
-    backgroundColor: 'teal'
+  },
+  day: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 178
+  },
+  description: {
+    marginTop: -20,
+    fontSize: 60
+    
   }
 })
 
