9acb1cf95dfcb35598e2ff252175438be15a504f
jkim8 <juno0218@naver.com>
2022-08-03 11:42:06 -0400

    using requestForegroundPermissionsAsync/getCurrentPositionAsync/reverseGeocodeAsync

--- App.js
+++ App.js
@@ -1,14 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { ScrollView, Dimensions, StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 
 export default function App() {
+  const [city, setCity] = useState('...Loading');
+  const [location, setLocation] = useState();
+  const [ok, setOk] = useState(true);
+  const ask = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      {
+        latitude,
+        longitude,
+      },
+      { useGoogleMaps: false },
+    );
+    setCity(location[0].city);
+  };
+  useEffect(() => {
+    ask();
+  }, []);
+
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
       <View style={styles.city}>
-        <Text style={styles.cityName}>Atlanta</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       <ScrollView
         pagingEnabled
