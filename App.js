7c01de9a1859c2c116cecda8d75067945fedcd31
jkim8 <juno0218@naver.com>
2022-08-03 09:41:18 -0400

    prettier apply

--- App.js
+++ App.js
@@ -5,34 +5,34 @@ import { Colors } from 'react-native/Libraries/NewAppScreen';
 export default function App() {
   return (
     <View style={styles.container}>
-    <StatusBar style="light"/>
-    <View style={styles.city}>
-      <Text style={styles.cityName}>Atlanta</Text>
-    </View>
-    <View style={styles.weather}>
-      <View style={styles.day}>
-        <Text style={styles.temp}>27</Text>
-        <Text style={styles.description}>Sunny</Text>
+      <StatusBar style="light" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Atlanta</Text>
       </View>
-    </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27°</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
-    backgroundColor: "tomato"
+    flex: 1,
+    backgroundColor: 'teal',
   },
   city: {
     flex: 1.2,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   cityName: {
     color: 'white',
     fontSize: 68,
-    fontWeight: '500'
+    fontWeight: '500',
   },
   weather: {
     flex: 3,
@@ -43,12 +43,12 @@ const styles = StyleSheet.create({
   },
   temp: {
     marginTop: 50,
-    fontSize: 178
+    fontSize: 158,
+    color: 'white',
   },
   description: {
     marginTop: -20,
-    fontSize: 60
-    
-  }
-})
-
+    fontSize: 60,
+    color: 'white',
+  },
+});
